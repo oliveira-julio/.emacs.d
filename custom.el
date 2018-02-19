dd1dd8a9d2e32923bdb36c7727bf5350cf8f39e0
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-02-19 02:26:12 -0300

    My config, include elisp files, custom-file and snippets

---
+++ custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(org-babel-load-languages (quote ((sh . t) (python . t) (emacs-lisp . t))))
+ '(org-drill-optimal-factor-matrix nil)
+ '(org-export-backends (quote (ascii html icalendar latex man md)))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill org-learn)))
+ '(package-selected-packages
+   (quote
+    (smart-mode-line ## use-package yasnippet-snippets elpy yasnippet virtualenvwrapper virtualenv org-drill-table org-link-minor-mode))))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,84 @@
+
+(require 'package)
+(package-initialize)
+(setq package-enable-at-startup nil)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+(setq user-full-name "Julio Oliveira"
+      user-mail-address "antoniojuliodeo@gmail.com")
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(add-to-list 'load-path "~/.emacs.d/elisp/themes/")
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; default directories
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+;; backup config
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elisp/themes/")
+(load-theme 'solarized-dark t)
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq inhibit-startup-screen  t
+initial-scratch-message nil)
+
+(line-number-mode   1)
+(column-number-mode 1)
+
+(global-hl-line-mode   1)
+(show-paren-mode 1)
+
+(sml/setup)
+
+(display-battery-mode)
+(display-time-mode)
+
+(require 'ido)
+(ido-mode t)
+
+(require 'linum)
+(require 'linum-relative)
+(global-linum-mode t)
+(linum-relative-global-mode t)
+
+(require 'org-learn)
+(require 'org-drill)
+(require 'org-bullets)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "|" "CANCELED(c)")))
+
+(setq org-log-done 'time)
+
+;; (setq org-agenda-files '("~/Dropbox/org/agenda"))
+
+(add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
+
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(require 'yasnippet)
+
+(yas-global-mode 1)
